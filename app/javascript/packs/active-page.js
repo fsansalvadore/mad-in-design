68fe7f9707061e30f8f67068218a77dc84fab6f9
fsansalvadore <f.sansalvadore@gmail.com>
2020-07-03 19:29:50 +0200

    fixed projects year filtering and active link selection

--- app/javascript/packs/active-page.js
+++ app/javascript/packs/active-page.js
@@ -1,6 +1,6 @@
 const ActivePage = () => {
   let url = window.location.href.split("/");
-  let url_path = url[3];
+  let url_path = url[3].split("?")[0];
   let currentNav = document.querySelector(`#navigation.main-menu a[href*='${url_path}'`);
   currentNav.classList.add("active");
 }

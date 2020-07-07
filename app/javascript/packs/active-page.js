6f5ab1dd74b826ca557bb56bbbe68ec07122788c
fsansalvadore <f.sansalvadore@gmail.com>
2020-07-07 15:09:26 +0200

    active page also on homepage now

--- app/javascript/packs/active-page.js
+++ app/javascript/packs/active-page.js
@@ -1,7 +1,12 @@
 const ActivePage = () => {
   let url = window.location.href.split("/");
   let url_path = url[3].split("?")[0];
-  let currentNav = document.querySelector(`#navigation.main-menu a[href*='${url_path}'`);
+  let currentNav = "";
+  if (url_path === "") {
+    currentNav = document.querySelector(`#navigation.main-menu a[href='/'`);
+  } else {
+    currentNav = document.querySelector(`#navigation.main-menu a[href*='${url_path}'`);
+  }
   currentNav.classList.add("active");
 }
 
